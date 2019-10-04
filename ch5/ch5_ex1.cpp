8f21d95550f0040d7bf68d436c511d594d3405c8
Dimitri <kudryashovda@gmail.com>
2019-10-04 23:13:38 +0300

    Create ch5_ex1.cpp

---
+++ ch5/ch5_ex1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+double ctok(double c) {
+    double k = c + 273.15;
+    return k;
+}
+
+int main() {
+    double c = 0;
+    cin >> c;
+    double k = ctok(c);
+    cout << k << endl ;
+}
