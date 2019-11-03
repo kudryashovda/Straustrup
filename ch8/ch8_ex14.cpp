1052b96499c4a527c41a955dac487e4fd01886e8
Dimitri <kudryashovda@gmail.com>
2019-11-03 14:31:39 +0300

    Update ch8_ex14.cpp

--- ch8/ch8_ex14.cpp
+++ ch8/ch8_ex14.cpp
@@ -4,11 +4,19 @@
 
 using namespace std;
 
-void foo(const vector<string>&, vector<int>&);
-
 void foo(const vector<string>& vs, vector<int>& vi) {
     for(auto it: vs)
         vi.push_back(it.size());
+}
+
+template <typename T>
+auto posOfMin(T& vs) {
+    return min_element(vs.begin(), vs.end());
+}
+
+template <typename T>
+auto posOfMax(T& vs) {
+    return max_element(vs.begin(), vs.end());
 }
 
 int main() {
@@ -21,27 +29,21 @@ int main() {
         "tea"
     };
 
+    for(auto it: vs) cout << it << endl;
+    cout << endl;
+
     vector<int> vi; // keep size of each string in vs vector
 
     foo(vs, vi);
 
-    vector<int>::iterator it0;
+    int d;
+    d = distance(vi.begin(), posOfMax(vi));
+    cout << "the longest string is " << vs[d] << endl;
 
-    it0 = max_element(vi.begin(), vi.end());
-    int dmax = distance(vi.begin(), it0);
-    cout << "the longest string is " << vs[dmax] << endl;
-
-    it0 = min_element(vi.begin(), vi.end());
-    int dmin = distance(vi.begin(), it0);
-    cout << "the shortest string is " << vs[dmin] << endl;
+    d = distance(vi.begin(), posOfMin(vi));
+    cout << "the shortest string is " << vs[d] << endl;
 
     // max-min for strings
-    vector<string>::iterator it;
-        
-    it = min_element(vs.begin(), vs.end());
-    cout << "the prim string is " << *it << endl;
-   
-    it = max_element(vs.begin(), vs.end());
-    cout << "the latest string is " << *it << endl;
-
+    cout << "the primary alfabetic string is " << *posOfMin(vs) << endl;
+    cout << "the latest alfabetic string is " << *posOfMax(vs) << endl;
 }
