cd1dc62eaf4c5ec7ae78745fae80f65b1e8650c4
Dimitri <kudryashovda@gmail.com>
2019-12-15 20:33:46 +0300

    Create book.h

---
+++ ch9/ex6/book.h
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Book
+{
+private:
+    string isbn;
+    string name;
+    string author;
+    string adate;
+    bool isOut;
+public:
+    string getISBN() const;
+    string getName() const;
+    string getAuthor() const;
+    string getAdate() const;
+    bool checkOut() const;
+
+    bool setISBN(string str);
+    bool setName(string str);
+    bool setAuthor(string str);
+    bool setAdate(string str);
+    bool setOut();
+    bool setIn();
+
+    bool checkISBN(string str) const;
+    bool checkAdate(string str) const;
+    vector<string> tokenizer(string str, string delim) const;
+    bool isNum(string str) const;
+};
+
+bool operator==(const Book& l, const Book& r);
+bool operator!=(const Book& l, const Book& r);
+
+ostream& operator<<(ostream& os, const Book& np);
