5364b749bce45edfd09abba2194275e74abb9959
Dimitri <kudryashovda@gmail.com>
2019-12-28 18:33:55 +0300

    Create ch9_ex7_main.cpp

---
+++ ch9/ex7/ch9_ex7_main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cassert>
+#include "book.h"
+
+using namespace std;
+
+int main()
+{
+    Book book1;
+
+    book1.setName("Moo-Moo");
+    book1.setAuthor("Turgenev");
+    book1.setISBN("12-33-21-A");
+    book1.setAdate("28.2.2001");
+    book1.setGenre(Book::fantastic);
+    book1.setIn();
+
+
+    Book book2;
+
+    book2.setName("Oblomob");
+    book2.setAuthor("Goncharov");
+    book2.setISBN("32-3-21-C");
+    book2.setAdate("12.6.1998");
+    book2.setGenre(Book::prosa);
+    book2.setIn();
+
+
+    assert(!(book1 == book2));
+    assert(book1 != book2);
+    
+    assert(cout << book1);
+    assert(cout << book2);
+}
